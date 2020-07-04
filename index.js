adcf5b5e287722f5119c0a9f5e97a7535e126f4e
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-05 02:10:28 +0530

    Add button

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-console.log('testing')
+console.log('testing TODOS')
+
+const todos = [{
+  text: 'Order cat food',
+  completed: false
+}, {
+  text: 'Clean kitchen',
+  completed: true
+}, {
+  text: 'Buy food',
+  completed: true
+}, {
+  text: 'Do work',
+  completed: false
+}, {
+  text: 'Exercise',
+  completed: true
+}]
 
 document.querySelectorAll('p').forEach(p => p.remove());
+
+const itemLeftCount = todos.filter(item => item.completed === false).length
+const itemsLeft = document.createElement('h2')
+const body = document.querySelector('body')
+
+itemsLeft.textContent = `You have ${itemLeftCount} todos left`
+body.append(itemsLeft)
+
+
+todos.forEach(item => {
+  const p = document.createElement('p')
+  p.textContent = item.text
+  body.appendChild(p)
+})
+
+document.querySelector('button').addEventListener('click', function (e) {
+  console.log(e.target.textContent = 'button clicked')
+})
